b8111816078c7c7089846e258b9d18eccb6aa96c
myriamroxanamedina <104227876+myriamroxanamedina@users.noreply.github.com>
2022-09-12 13:28:02 -0300

    Add files via upload

---
+++ tp1_backend.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Punto 1</h1>
+    <?php
+      echo "Hola Mundo";
+     ?>
+     <h1>Punto 2</h1>
+     <?php
+     $saludo = "Hola Mundo";
+     echo $saludo;
+      ?>
+  </body>
+</html>
